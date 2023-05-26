84b43cccb84eb96cb7dd47c7ae6421da4cc9086f
DariaCherkasova <106240145+DariaCherkasova@users.noreply.github.com>
2023-05-26 16:39:06 +0400

    init

---
+++ echo_server_1/client.py
@@ -0,0 +1,29 @@
+import socket
+
+sock = socket.socket()
+
+while True:
+    try:
+        address = input("Введите адрес сервера: ")
+        if address == '':
+            address = 'localhost'
+        while True:
+            port = int(input("Введите порт (диапозон 1024-65535): "))
+            if 1024 <= port <= 65535:
+                break
+        sock.connect((address, port))
+    except (socket.error, ValueError):
+        print("Повторите ввод!")
+    else:
+        break
+
+# массив для имитации сообщений от пользователя
+msg = ["Привет", "Как дела?", "exit"]
+
+# Цикл для имитации ввода сообщений пользователем
+for i in range(len(msg)):
+    sock.send(msg[i].encode())
+    data = sock.recv(1024)
+    print(data.decode())
+
+sock.close()
